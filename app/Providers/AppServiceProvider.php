a052ae85f57b516976ff19796b779d8e8dc70491
lamino111 <laminomamino@gmail.com>
2019-06-26 13:45:21 +0100

    ajout du template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,36 @@
 <?php
 
+// namespace App\Providers;
+
+// use Illuminate\Support\ServiceProvider;
+
+// class AppServiceProvider extends ServiceProvider
+// {
+//     *
+//      * Bootstrap any application services.
+//      *
+//      * @return void
+     
+//     public function boot()
+//     {
+//         //
+//     }
+
+//     /**
+//      * Register any application services.
+//      *
+//      * @return void
+//      */
+//     public function register()
+//     {
+//         //
+//     }
+// }
 namespace App\Providers;
-
+ 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
+ 
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,9 +40,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
-
+ 
     /**
      * Register any application services.
      *
